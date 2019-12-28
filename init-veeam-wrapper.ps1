fae700ccd717e86a0dbabd157ed62dc073fcd4a2
Paul Drangeid <pdrangeid@gmail.com>
2019-12-28 10:04:54 -0600

    updated documentation

--- init-veeam-wrapper.ps1
+++ init-veeam-wrapper.ps1
@@ -1,6 +1,6 @@
 <# 
 .SYNOPSIS 
- Connect to the veeam webAPI to populate backup data in a Neo4j GraphDB
+ Connect to the Veeam Backup Enterprise Manager webAPI to populate backup data in a Neo4j GraphDB
  
 .DESCRIPTION 
 By providing the Veeam API URL, the credentials for Veeam and Neo4j, a session will be opened to populate your database
@@ -9,8 +9,8 @@ with labels, properties and relationships between Veeam servers, jobs, restorati
 example:
 ./init-veeam-wrapper.ps1 -baseapiurl "htttp://myveeamserver.mydomain.com:9399" -veeamcred myveeam -neo4jdatasource myneo4jserver -days 7
 
-The veeamcred and neo4jdatasource profiles must have already been created using the set-regcredentials.ps1 and must have been
-run as the same user account that is running init-veeam-wrapper.ps1
+The veeamcred and neo4jdatasource profiles must have previously been configured using the set-regcredentials.ps1 and must have been
+run as the same user account that is now running init-veeam-wrapper.ps1
 
 .NOTES 
 ┌─────────────────────────────────────────────────────────────────────────────────────────────┐ 
@@ -26,9 +26,9 @@ run as the same user account that is running init-veeam-wrapper.ps1
 │               : -verbosity                   :Level of on-screen messaging (0-4)            │ 
 │               : -days                        :If 1st run how many days of backups to query  │ 
 │   PREREQS     :                                                                             │ 
-│               : RVTools (https://www.robware.net/rvtools/) v 3.11.6 or newer                │ 
-│               : Other scrpit modules:                                                       │ 
-│               : see SITE above for other modules needed to run this                         │ 
+│               : Credentials for the Veeam Backup Enterprise Manager webAPI                  │ 
+│               : Other script modules:                                                       │ 
+│               : see blog SITE above for other modules needed to run this                    │ 
 └─────────────────────────────────────────────────────────────────────────────────────────────┘ 
 #> 
 
@@ -65,31 +65,39 @@ $Path = "HKCU:\Software\neo4j-wrapper\Credentials\$veeamcred"
 $veeamuser=Ver-RegistryValue -RegPath $Path -Name $($veeamcred+"User")
 $veeampw=Get-SecurePassword $Path $($veeamcred+"PW")
 $pair = "$($veeamuser):$($veeampw)"
-Remove-Variable -name veeampw | Out-Null
+
+# Now we have the Veeam username & password.  We need to convert them to base64 to use as a parameter when querying the 
+# Veeam Backup Enterprise Manager webAPI
 $encodedveeamCreds=[System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+Remove-Variable -name veeampw | Out-Null
+Remove-Variable -name veeamuser | Out-Null
 Remove-Variable -name pair | Out-Null
 
+# Let's open a WebRequest to get a valid sessionkey so we can interact with the Veeam Backup Enterprise Manager webAPI
 $apiurl="$baseapiurl/sessionMngr/?v=latest"
 $response=(Invoke-WebRequest $apiurl -Method 'POST' -Headers @{"Authorization"="Basic $encodedveeamCreds";Accept="application/json"} -ErrorVariable RestError)
 $veeamapisession=$($response.headers["X-RestSvcSessionId"])
-
 Remove-Variable -name encodedveeamCreds | Out-Null
 
 if ($sessionkey -eq $true){
-    #$sessionkey switch indicates I just want to generate and return a valid session key.
+    #$sessionkey switch tells this script you just want to generate and return a valid session key.
     #This is a quick way to get a key so you can run your own manual queries for testing.
     write-host "Session id: $veeamapisession"
     exit
 }
 
+# Now that we have the sessionkey, let's run the refresh-veeam (or init-veeam if -init switch) cypher script against the n4j database
+# The get-cypher-results.ps1 will execute and show the logged transaction results.
 $scriptpath = -join ($PSScriptRoot,"\get-cypher-results.ps1")
 $csp= -join ($PSScriptRoot,'\refresh-veeam.cypher')
 if ($init -eq $true){
 $csp= -join ($PSScriptRoot,'\init-veeam.cypher')}
 $findstring='{"base-veeam-api-url":"'+$baseapiurl+'","veeam-restsvc-sessionid":"'+$veeamapisession+'","restorepointsmaxage":"'+$days+'"}'
 Show-onscreen $("`nExecuting the following:`n. $scriptPath -Datasource $neo4jdatasource -cypherscript $csp -logging $neo4jdatasource -findrep $findstring -verbosity $verbosity`n ") 4
-#$result = . $scriptPath -Datasource $neo4jdatasource -cypherscript $csp -logging $neo4jdatasource -findrep $findstring -verbosity 4
 . $scriptPath -Datasource $neo4jdatasource -cypherscript $csp -logging $neo4jdatasource -findrep $findstring -verbosity $verbosity
 
 Remove-Variable -name veeamapisession | Out-Null
+Remove-Variable -name findstring | Out-Null
+
+# All done!
 
